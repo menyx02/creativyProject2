78c04ec85a3280bb63fb7d2a329c336c7a80daf1
Manuel Marquez <meny_marquez@hotmail.com>
2018-02-14 21:16:23 -0700

    thing is working now. Left -> format of results and general format

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ $(document).ready(function () {
 
 
     $("#searchSubmit").click(function(e) {
+        e.preventDefault();
 
         var dropdown = document.getElementById("category");
         var id = dropdown.options[dropdown.selectedIndex].value;
@@ -9,28 +10,44 @@ $(document).ready(function () {
 
         //Error checking
         if($("#itemInput").val() == "" ) {
-            e.preventDefault();
             alert("Please type something to search for");
             return;
         }
         if(id == 0){
-            e.preventDefault();
             alert("Please select a category");
             return;
         }
 
+        var BASE_URL = "http://api.walmartlabs.com/v1/search?query=";
+        var query = $("#itemInput").val();
+        //format the query to the format required by WALMART
+        query = query.replace(' ', "+");
 
-        var myurl = "http://api.walmartlabs.com/v1/search?query=ipod&format=json&apiKey=nqk69bn333pb7j6tyu6e98xb";
+        var myurl = BASE_URL + query + "&format=json&apiKey=nqk69bn333pb7j6tyu6e98xb";
         $.ajax({
             url: myurl,
             dataType: "jsonp",
             success: function (json) {
+                var results = "";
+
+                if(json.numItems == 0) {
+                    results = "Sorry, no items matched this search. Try again";
+                    $("#searchResults").html(results);
+                    return;
+                }
+
+
+
                 console.log(json);
                 console.log(json.items[0].name);
                 console.log(json.items[0].salePrice);
                 console.log(json.items[0].shortDescription);
             }
         });
+    });
+
+    $("#newSearch").click(function (e) {
+
     });
 
 });
